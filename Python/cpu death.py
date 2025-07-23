7403a67b0ab9a6b0c58e59b8d1913ed02b130414
TheSquiggle <silas.fierro@gmail.com>
2025-07-23 11:35:30 -0500

    Update cpu death.py

--- Python/cpu death.py
+++ Python/cpu death.py
@@ -10,4 +10,7 @@ folder = os.path.dirname(__file__)
 while True:
     for filename in os.listdir(folder):
         if filename.endswith(".py") and filename != os.path.basename(__file__):
-            subprocess.Popen(["python", os.path.join(folder, filename)], shell=True)
+            subprocess.Popen(
+                ["start", "python", os.path.join(folder, filename)],
+                shell=True
+            )  # Opens each script in a new command prompt window
