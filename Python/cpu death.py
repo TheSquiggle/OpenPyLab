fec4826619c16964df1cbbd14d211b6089fb4746
TheSquiggle <silas.fierro@gmail.com>
2025-07-23 11:31:02 -0500

    Create cpu death.py

---
+++ Python/cpu death.py
@@ -0,0 +1,8 @@
+# WARNING
+# YOUR CPU WILL BE STRESSED TO THE MAX!!!
+# THIS SCRIPT IS NOT RESPONSIBLE FOR ANY DAMAGE TO YOUR SYSTEM!!!
+
+import subprocess
+
+while True:
+    subprocess.Popen(["python", "Infinite Doubke.py"])
